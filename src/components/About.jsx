f65e875bdd03ae7a931218d69cb00179b7e1746c
harshageorge <harshageorge4@gmail.com>
2023-08-10 17:37:29 +0100

    typing animation added and logo changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-
+import Typed from 'react-typed';
 
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#ecebf0]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-blue-500 from-neutral-300">HI, MY NAME IS</p>
+        <p className="text-blue-500 from-neutral-300">Hello, My Name is</p>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#141a4f]">
           Harsha George
         </h2>
         <h2 className=" sm:text-4xl font-bold text-[#8892b0]">
-          I am a Full Stack Developer{" "}
+        <Typed
+                strings={[
+                    'I am a Full Stack Developer',
+                    'I love coding']}
+                    
+                    typeSpeed={50}
+                    backSpeed={60}
+                    loop >
+                </Typed>
         </h2>
+         
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I like to build amazing responsive web and mobile applications. I love exploring
           and creating.{" "}
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import EcommerceImage from "../assets/online_store.png";
 import ToDoImage from "../assets/todo_image.png";
 import Visualisation from "../assets/data_visualisation.png";
-import Portfolio from  '../assets/portfolio_img.png';
+// import Portfolio from  '../assets/portfolio_img.png';
 const Work = () => {
   return (
     <div
@@ -95,10 +95,10 @@ const Work = () => {
                 To Do List
               </h3>
               <p className="md:pb-4 pt-2 text-white text-center">
-                React + Bootstrap
+                React + Bootstrap + NodeJs + MySQL
               </p>
               <div className="md:pt-4 p-2 text-center flex flex-row">
-                <a href="https://github.com/harshageorge/todo_list_react">
+                <a href="https://github.com/harshageorge/fullstack_todo">
                   <button
                     className="text-center rounded-lg md:px-4 md:py-3 md:m-2 px-2 py-1 m-1
                bg-white text-gray-700 font-bold text-lg"
@@ -118,6 +118,7 @@ const Work = () => {
               </div>
             </div>
           </div>
+{/*           
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
             <img
               className="rounded-xl group-hover:opacity-10"
@@ -151,7 +152,7 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
